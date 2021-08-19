9c6bc6670586366da9dce973219e67f42bf0cd9d
Alexander Shukan <avshukan@gmail.com>
2021-08-19 12:02:50 +1100

    apply: eslint and new npm

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const axios = require('axios');
 
 // axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
 axios.get('https://nsi.rosminzdrav.ru:443/port/rest/data?userKey=90af2cf9-b51d-4857-b494-c2cd523b25a3&identifier=1.2.643.5.1.13.13.11.1461&page=1&size=200')
-    .then(response => {
-        console.log('list.length', response.data.list.length);
-        // console.log(response.data);
-        // console.log(response.data.url);
-        // console.log(response.data.explanation);
-    })
-    .catch(error => {
-        console.log(error);
-    });
+  .then((response) => {
+    console.log('list.length', response.data.list.length);
+    // console.log(response.data);
+    // console.log(response.data.url);
+    // console.log(response.data.explanation);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
