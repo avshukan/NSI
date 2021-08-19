17d676a0fea38bd71e45c775e075f15bc1d38f1f
Alexander Shukan <avshukan@gmail.com>
2021-08-19 16:32:23 +1100

    add: getOnePage and getAllPages functions

--- index.js
+++ index.js
@@ -1,13 +1,45 @@
 const axios = require('axios');
+const dotenv = require('dotenv');
 
-// axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
-axios.get('https://nsi.rosminzdrav.ru:443/port/rest/data?userKey=90af2cf9-b51d-4857-b494-c2cd523b25a3&identifier=1.2.643.5.1.13.13.11.1461&page=1&size=200')
-  .then((response) => {
+dotenv.config();
+const { USERKEY, DICTIONARY } = process.env;
+
+async function getOnePage(userkey, dictionary, page = 1, size = 200) {
+  const url = `https://nsi.rosminzdrav.ru:443/port/rest/data?userKey=${userkey}&identifier=${dictionary}&page=${page}&size=${size}`;
+  const result = [];
+  try {
+    const response = await axios.get(url);
     console.log('list.length', response.data.list.length);
-    // console.log(response.data);
-    // console.log(response.data.url);
-    // console.log(response.data.explanation);
-  })
-  .catch((error) => {
+    result.push(...response.data.list);
+  } catch (error) {
     console.log(error);
-  });
+  }
+  return result;
+}
+
+async function getAllPages(userkey, dictionary) {
+  const result = [];
+  let list = [];
+  let page = 0;
+  const size = 200;
+  /* eslint-disable no-await-in-loop */
+  do {
+    try {
+      list = await getOnePage(userkey, dictionary, page, size);
+      result.push(...list);
+    } catch (error) {
+      console.log(error);
+      list = [];
+    }
+    page += 1;
+  } while (list.length === size);
+  /* eslint-enable no-await-in-loop */
+  return result;
+}
+
+(async () => {
+  const result = await getAllPages(USERKEY, DICTIONARY);
+  console.log('result.length', result.length);
+  console.log('result[0]', result[0]);
+  console.log('result[result.length-1]', result[result.length - 1]);
+})();
